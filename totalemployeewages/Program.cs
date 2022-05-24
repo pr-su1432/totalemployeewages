8ba0a191d8bfd26bae1d70133042a678fca25280
[prasanna] <prasanna>
2022-05-24 16:58:53 +0530

    [prasanna]: employee is present or not

---
+++ totalemployeewages/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace employeewages
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            totalemployeewages.ispresent value = new totalemployeewages.ispresent();
+            value.presentorabsent();
+        }
+    }
+}
